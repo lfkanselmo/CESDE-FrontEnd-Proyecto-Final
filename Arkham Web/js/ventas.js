489f43655e62b7a26dfc25f004671c4ab74c53d9
lfkanselmo <lfkanselmo@gmail.com>
2023-12-04 12:19:10 -0500

    Update ventas.js

--- Arkham Web/js/ventas.js
+++ Arkham Web/js/ventas.js
@@ -4,10 +4,16 @@ window.addEventListener("DOMContentLoaded", ()=>{
 })
 
 
-function cargarInmueblesEnVenta(){
+async function cargarInmueblesEnVenta(){
     let urlInmuebles = "http://localhost:8090/api/property/offer/VENTA";
 
-    fetch(urlInmuebles){
-        
-    }
+   await fetch(urlInmuebles)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(error => console.error(error));
+
+}
+
+function cargaInmueble(inmuebles){
+    
 }
